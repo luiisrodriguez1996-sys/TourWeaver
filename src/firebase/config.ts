1565486b8f71e3aa33e855582d3d2cba145da72f
Luis Rodriguez <luiisrodriguez1996@gmail.com>
2026-02-04 01:38:33 +0000

    Notificación	Alerta de actividad sospechosa El proyecto Firebase app (id

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9776081687-fec5d",
-  "appId": "1:330012524252:web:cd919be67886c4e6ff3e19",
-  "apiKey": "AIzaSyD0DEzJIQORiMgTzsJd5PQIomCHn3FP-Z0",
-  "authDomain": "studio-9776081687-fec5d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "330012524252"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-9776081687-fec5d",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:330012524252:web:cd919be67886c4e6ff3e19",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyD0DEzJIQORiMgTzsJd5PQIomCHn3FP-Z0",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-9776081687-fec5d.firebaseapp.com",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "330012524252"
 };
